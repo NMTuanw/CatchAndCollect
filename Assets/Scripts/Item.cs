093da9f613db7bac7cc2b0d914010100f0a42e86
ManhTuans <obmhenry12345@gmail.com>
2024-04-25 17:28:44 +0700

    Add Health System

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -19,37 +19,21 @@ public class Item : MonoBehaviour
     [SerializeField] public string itemDescription;
 
     private InventoryManager inventoryManager;
+
+    private HealthManager healthManager;
     private void Start() {
         inventoryManager = GameObject.Find(INVENTORYCANVAS).GetComponent<InventoryManager>(); 
+        healthManager = GameObject.Find("HealthManager").GetComponent<HealthManager>();
     }
     private void Update() {
         transform.Translate(Vector2.down * droppingSpeed * Time.deltaTime);
     }
 
-    // private void OnCollisionEnter2D(Collision2D other) {
-    //     if (other.gameObject.CompareTag("Ground"))
-    //     {
-    //         Destroy(gameObject);
-    //         Debug.Log("Prob touched the ground.");
-    //     }
-
-    //     if (other.gameObject.CompareTag("Player"))
-    //     {   
-    //         int leftOverItems = inventoryManager.AddItem(itemName, itemIcon, quantity, itemDescription);
-    //         if (leftOverItems <= 0)
-    //         {
-    //             Destroy(gameObject);
-    //         } else {
-    //             quantity = leftOverItems;
-    //         }
-    //         Debug.Log("Prob touched the player.");
-    //     }
-    // }
-
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Ground"))
         {
             Destroy(gameObject);
+            healthManager.RemoveHealth(1);
             Debug.Log("Prob touched the ground.");
         }
 
